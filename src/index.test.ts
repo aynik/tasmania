72ef769bb11e96922d63dad14f28ae5078554c05
Pablo Vazquez <aynik@protonmail.com>
2021-05-18 23:22:56 +0900

    Removed explicit transitions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,62 +1,52 @@
-import { createReducer, States } from '.';
+import { createReducer } from '.';
 
 describe('createReducer', () => {
-    it('calls the next state', async () => {
-        const nextState = jest.fn();
-        enum state {
-            A = 'A',
-            B = 'B',
-        }
-        enum transition {
-            CALL = 'CALL',
-        }
-        const transitions = {
-            [state.A]: {
-                [transition.CALL]: state.B,
-            },
-            [state.B]: null,
-        };
-        const states: States<typeof transitions, void> = {
-            [state.A]: async () => {
-                return { type: transition.CALL };
-            },
-            [state.B]: nextState,
-        };
-        const reduce = createReducer(transitions, states);
-        await reduce({ type: state.A });
-        expect(nextState).toHaveBeenCalled();
+  it('calls the next state', async () => {
+    const nextState = jest.fn();
+    enum state {
+      A = 'A',
+      B = 'B',
+    }
+    const reduce = createReducer<
+      {
+        [state.A]: state.B;
+        [state.B]: null;
+      },
+      null
+    >({
+      [state.A]: () => {
+        return { type: state.B, payload: null };
+      },
+      [state.B]: nextState,
     });
+    reduce({ type: state.A, payload: null });
+    expect(nextState).toHaveBeenCalled();
+  });
 
-    it('calls the next state with payload', async () => {
-        const endState = jest.fn();
-        enum state {
-            A = 'A',
-            B = 'B',
-            END = 'END',
-        }
-        enum transition {
-            CALL = 'CALL',
-        }
-        const transitions = {
-            [state.A]: {
-                [transition.CALL]: state.B,
-            },
-            [state.B]: {
-                [transition.CALL]: state.END,
-            },
-            [state.END]: null,
-        };
-        const states: States<typeof transitions, { ended: boolean }> = {
-            [state.A]: async () => {
-                return { type: transition.CALL };
-            },
-            [state.B]: async () => {
-                return { type: transition.CALL, payload: { ended: true } };
-            },
-            [state.END]: endState,
-        };
-        const reduce = createReducer(transitions, states);
-        await reduce({ type: state.A });
-        expect(endState).toHaveBeenCalledWith({ ended: true });
+  it('calls the next state with payload', async () => {
+    const endState = jest.fn();
+    enum state {
+      A = 'A',
+      B = 'B',
+      END = 'END',
+    }
+    const reduce = createReducer<
+      {
+        [state.A]: state.B;
+        [state.B]: state.END;
+        [state.END]: null;
+      },
+      { ended: boolean }
+    >({
+      [state.A]: () => {
+        return { type: state.B, payload: { ended: false } };
+      },
+      [state.B]: () => {
+        return { type: state.END, payload: { ended: true } };
+      },
+      [state.END]: endState,
     });
+    reduce({ type: state.A, payload: { ended: false } });
+    expect(endState).toHaveBeenCalledWith({ ended: true });
+  });
 });
